b4978c09a56f579e0404768f33e5e21fd2e57ff1
khairnarabhijeet <47552955+khairnarabhijeet@users.noreply.github.com>
2024-08-22 19:30:38 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 import streamlit as st
 from transformers import AutoModelForCausalLM, AutoTokenizer
-import torch
+#import torch
 
 # Load the pre-trained model and tokenizer
 model_name = "microsoft/DialoGPT-medium"
@@ -8,19 +8,19 @@ model = AutoModelForCausalLM.from_pretrained(model_name)
 tokenizer = AutoTokenizer.from_pretrained(model_name)
 
 # Function to generate chatbot responses
-def get_chatbot_response(user_input):
+#def get_chatbot_response(user_input):
     # Encode the user input and add the EOS token
-    inputs = tokenizer.encode(user_input + tokenizer.eos_token, return_tensors="pt")
+    #inputs = tokenizer.encode(user_input + tokenizer.eos_token, return_tensors="pt")
     
     # Create an attention mask: 1 for tokens that are not padding
-    attention_mask = torch.ones(inputs.shape, dtype=torch.long)
+    #attention_mask = torch.ones(inputs.shape, dtype=torch.long)
     
     # Generate the response with attention_mask
-    response_ids = model.generate(inputs, attention_mask=attention_mask, max_length=1000, pad_token_id=tokenizer.eos_token_id)
+    #response_ids = model.generate(inputs, attention_mask=attention_mask, max_length=1000, pad_token_id=tokenizer.eos_token_id)
     
     # Decode and return the response
-    response = tokenizer.decode(response_ids[:, inputs.shape[-1]:][0], skip_special_tokens=True)
-    return response
+    #response = tokenizer.decode(response_ids[:, inputs.shape[-1]:][0], skip_special_tokens=True)
+    #return response
 
 # Streamlit App Interface
 st.title("Abhijeet K - Program Manager")
@@ -73,11 +73,11 @@ st.write("""
 """)
 
 # Chatbot Section
-st.header("Chat with Me!")
-user_input = st.text_input("Ask a question about me:")
+#st.header("Chat with Me!")
+#user_input = st.text_input("Ask a question about me:")
 
-if user_input:
-    response = get_chatbot_response(user_input)
-    st.write(response)
+#if user_input:
+ #   response = get_chatbot_response(user_input)
+  #  st.write(response)
 
-st.write("Feel free to ask any question related to my experience, skills, or anything you'd like to know!")
+#st.write("Feel free to ask any question related to my experience, skills, or anything you'd like to know!")
